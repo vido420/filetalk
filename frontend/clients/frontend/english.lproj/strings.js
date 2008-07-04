5de9d16b9eeea4bfa42e52b779651d8e2eaafd0c
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-04 15:38:08 +0000

    initial frontend

---
+++ frontend/clients/frontend/english.lproj/strings.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Frontend English Strings
+// ==========================================================================
+
+// Place strings you want to localize here.  In your app, use the key and
+// localize it using "key string".loc().  HINT: For your key names, use the
+// english string with an underscore in front.  This way you can still see
+// how your UI will look and you'll notice right away when something needs a
+// localized string added to this file!
+//
+Object.extend(String.English,{
+  // "_String Key": "Localized String"
+  "Login_Account_Details": "Account Details",
+  "Login_Name": "Login",
+  "Login_Password": "Password",
+  "Login_Button": "Login",
+}) ;
