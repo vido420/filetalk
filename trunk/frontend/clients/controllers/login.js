c3c7babb87a418a72edd606dac4657171ed3b358
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-04 15:38:08 +0000

    initial frontend

---
+++ trunk/frontend/clients/controllers/login.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// LoginController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+loginController = SC.Object.create(
+/** @scope loginController */ {
+
+  // TODO: Add your own code here.
+	login: function() {
+		alert('Hello World!');
+	}
+}) ;
---
+++ trunk/frontend/clients/models/user.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// User
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+User = SC.Record.extend(
+/** @scope User.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
