d3ba1837f7a20fbcba0cad9cdc9597d744833b78
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-05 22:36:14 +0000

    userlist an actual list_view

---
+++ frontend/clients/frontend/controllers/userlist.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Frontend.UserlistController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+Frontend.userlistController = SC.ArrayController.create(
+/** @scope Frontend.userlistController */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ frontend/clients/frontend/models/user.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Frontend.User
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+Frontend.User = SC.Record.extend(
+/** @scope Frontend.User.prototype */ {
+
+  // TODO: Add your own code here.
+  socket: Number,
+//  icon: Number,
+  status: Number,
+
+}) ;
+
