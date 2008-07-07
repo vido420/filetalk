e9522ac43c667de47cd0b0645cc1930a0e63d4c5
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-07 02:41:18 +0000

    chat model & controller & view & stuff

---
+++ trunk/frontend/clients/frontend/models/chat_message.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// Frontend.ChatMessage
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+Frontend.ChatMessage = SC.Record.extend(
+/** @scope Frontend.ChatMessage.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
