a7f0cc3519bf16f1095e93b262ad147e33a11817
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-09 04:31:45 +0000

    logging in

--- backend/app/controllers/login_controller.rb
+++ backend/app/controllers/login_controller.rb
@@ -1,6 +1,6 @@
 class LoginController < ApplicationController
   def index
-    if params['name'] == 'Myrd'
+    if Hotline::login(session, params['name'], params['pass'])
       render :text => "Hello" and return
     else
       render :text => 'Invalid login.', :status => 500 and return
