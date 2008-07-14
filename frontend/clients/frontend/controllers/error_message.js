8aa4964ddcd806d2dcd1e0328f0cbe51a4db1ba1
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-14 02:32:20 +0000

    handle disconnects

--- frontend/clients/frontend/controllers/error_message.js
+++ frontend/clients/frontend/controllers/error_message.js
@@ -17,12 +17,16 @@ Frontend.errorMessageController = SC.Object.create(
 /** @scope Frontend.errorMessageController */ {
 	errorTitle: 'Error',
 	errorMessage: 'Error Message',
-	showErrorDialog: function(title, message) {
+	errorCloseAction: null,
+	showErrorDialog: function(title, message, closeAction) {
 		Frontend.errorMessageController.set('errorTitle', title);
 		Frontend.errorMessageController.set('errorMessage', message);
+		Frontend.errorMessageController.set('errorCloseAction', closeAction);
 		SC.page.get('errorDialog').set('isVisible', true);
 	},
 	hideErrorDialog: function() {
 		SC.page.get('errorDialog').set('isVisible', false);
+		var closeAction = Frontend.errorMessageController.get('errorCloseAction');
+		if (closeAction) { closeAction(); }
 	},
 });
