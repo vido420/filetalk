0fc2867996099f0d00cc10a925b4eeb657e1f21b
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-07-20 05:52:13 +0000

    change updating

--- trunk/frontend/clients/frontend/controllers/app.js
+++ trunk/frontend/clients/frontend/controllers/app.js
@@ -29,11 +29,10 @@ Frontend.appController = SC.Object.create(
 			SC.page.get('contentView').set('content', SC.page.get('loginView'));
 		}
 	}.observes('tab'),
+	isPolling: false,
 	startPolling: function() {
-		if (!this._timer) this._timer = SC.Timer.schedule({
-			target: this, action: 'poll', repeats: true, interval: 1000
-		});
-		this._timer.set('isPaused', false);
+		this.set('isPolling', true);
+		Frontend.appController.poll();
 	},
 	poll: function() {
 		var request = new Ajax.Request('/backend/update', {
@@ -43,91 +42,97 @@ Frontend.appController = SC.Object.create(
 			onSuccess: function(response) {
 				var json = '' + response.transport.responseText;
 				json = eval(json);
-				var records = [];
-				var users = Frontend.User.findAll();
-				var hadMessages = false;
-				var chatView = SC.page.getPath('chatView.chatHistoryScrollView.chatHistoryView');
-				var chatHTML = chatView.get('innerHTML');
-				for (var i = 0; i < json.length; i++) {
-					var record = json[i];
-					if (record.recordType == 'clear_userlist') {
-						Frontend.User.removeAll();
-					} else if (record.recordType == 'user_left') {
-						var user = Frontend.User.find(record.guid);
-						if (user != null) {
-							var msg = '<<< ' + user.get('nick') + ' has left >>>';
-							chatHTML += '<div class="info">&nbsp;' + msg.escapeHTML() + '</div>';
-							hadMessages = true;
-							SC.Store.removeRecord(user);
-						}
-					} else if (record.recordType == 'chat') {
-						var msg = record.message.escapeHTML();
-						for (var i = 0; i < msg.length; i++) {
-							if (msg.charAt(i) != ' ') {
-								msg = msg.substring(i);
-								for (; i > 0; i--) {
-									msg = '&nbsp;' + msg;
-								}
-								break;
-							}
-						}
-						chatHTML += '<div class="msg">' + msg + '</div>';
-						hadMessages = true;
-					} else if (record.recordType == 'pm') {
-						chatHTML += '<div class="pm">';
-						chatHTML += '<div class="head">';
-						chatHTML += 'Private Message from ' + record.nick.escapeHTML() + ':';
-						chatHTML += '</div>';
-						chatHTML += '<div class="msg">' + record.message.escapeHTML() + '</div>';
-						chatHTML += '</div>';
-						hadMessages = true;
-					} else {
-						if (record.recordType == 'user') {
-							record.recordType = Frontend.User;
-							if ((record.status & 2) == 2) {
-								record.icon = '/images/adminuser.png';								
-							} else {
-								record.icon = '/images/reguser.png';
-							}
+				if (json) {
+					var records = [];
+					var users = Frontend.User.findAll();
+					var hadMessages = false;
+					var chatView = SC.page.getPath('chatView.chatHistoryScrollView.chatHistoryView');
+					var chatHTML = chatView.get('innerHTML');
+					for (var i = 0; i < json.length; i++) {
+						var record = json[i];
+						if (record.recordType == 'clear_userlist') {
+							Frontend.User.removeAll();
+						} else if (record.recordType == 'user_left') {
 							var user = Frontend.User.find(record.guid);
-							if (user) {
-								if (user.get('nick') != record.nick) {
-									var msg = '<<< ' + user.get('nick') + ' is now known as ' + record.nick + ' >>>';
+							if (user != null) {
+								var msg = '<<< ' + user.get('nick') + ' has left >>>';
+								chatHTML += '<div class="info">&nbsp;' + msg.escapeHTML() + '</div>';
+								hadMessages = true;
+								SC.Store.removeRecord(user);
+							}
+						} else if (record.recordType == 'chat') {
+							var msg = record.message.escapeHTML();
+							for (var i = 0; i < msg.length; i++) {
+								if (msg.charAt(i) != ' ') {
+									msg = msg.substring(i);
+									for (; i > 0; i--) {
+										msg = '&nbsp;' + msg;
+									}
+									break;
+								}
+							}
+							chatHTML += '<div class="msg">' + msg + '</div>';
+							hadMessages = true;
+						} else if (record.recordType == 'pm') {
+							chatHTML += '<div class="pm">';
+							chatHTML += '<div class="head">';
+							chatHTML += 'Private Message from ' + record.nick.escapeHTML() + ':';
+							chatHTML += '</div>';
+							chatHTML += '<div class="msg">' + record.message.escapeHTML() + '</div>';
+							chatHTML += '</div>';
+							hadMessages = true;
+						} else {
+							if (record.recordType == 'user') {
+								record.recordType = Frontend.User;
+								if ((record.status & 2) == 2) {
+									record.icon = '/images/adminuser.png';								
+								} else {
+									record.icon = '/images/reguser.png';
+								}
+								var user = Frontend.User.find(record.guid);
+								if (user) {
+									if (user.get('nick') != record.nick) {
+										var msg = '<<< ' + user.get('nick') + ' is now known as ' + record.nick + ' >>>';
+										chatHTML += '<div class="info">&nbsp;' + msg.escapeHTML() + '</div>';
+										hadMessages = true;
+									}
+								} else if (users && users.length > 0) {
+									/* Don't display these when users is empty => aka when just joining. */
+									var msg = '<<< ' + record.nick + ' has joined >>>';
 									chatHTML += '<div class="info">&nbsp;' + msg.escapeHTML() + '</div>';
 									hadMessages = true;
 								}
-							} else if (users && users.length > 0) {
-								/* Don't display these when users is empty => aka when just joining. */
-								var msg = '<<< ' + record.nick + ' has joined >>>';
-								chatHTML += '<div class="info">&nbsp;' + msg.escapeHTML() + '</div>';
-								hadMessages = true;
 							}
+							records.push(record);
 						}
-						records.push(record);
+					}
+					SC.Store.updateRecords(records);
+			  		Frontend.userlistController.set('content', Frontend.User.collection().refresh());
+					if (hadMessages) {
+						chatView.set('innerHTML', chatHTML);
+						/* Scroll to the bottom or keep same position... */
+						var scrollView = SC.page.getPath('chatView.chatHistoryScrollView').rootElement;
+						var currentHeight = 0;
+
+						if (scrollView.scrollHeight > 0) {
+							currentHeight = scrollView.scrollHeight;
+						} else if (scrollView.offsetHeight > 0) {
+							currentHeight = scrollView.offsetHeight;
+						}
+
+						var height = ((scrollView.style.pixelHeight) ? scrollView.style.pixelHeight : scrollView.offsetHeight);
+				    	if (currentHeight - scrollView.scrollTop - height < 25) {
+				        	scrollView.scrollTop = currentHeight;
+						}
 					}
 				}
-				SC.Store.updateRecords(records);
-			  	Frontend.userlistController.set('content', Frontend.User.collection().refresh());
-				if (hadMessages) {
-					chatView.set('innerHTML', chatHTML);
-					/* Scroll to the bottom or keep same position... */
-					var scrollView = SC.page.getPath('chatView.chatHistoryScrollView').rootElement;
-					var currentHeight = 0;
-
-					if (scrollView.scrollHeight > 0) {
-						currentHeight = scrollView.scrollHeight;
-					} else if (scrollView.offsetHeight > 0) {
-						currentHeight = scrollView.offsetHeight;
-					}
-
-					var height = ((scrollView.style.pixelHeight) ? scrollView.style.pixelHeight : scrollView.offsetHeight);
-				    if (currentHeight - scrollView.scrollTop - height < 25) {
-				        scrollView.scrollTop = currentHeight;
-					}
+				var isPolling = Frontend.appController.get('isPolling');
+				if (isPolling) {
+					var timer = SC.Timer.schedule({ target: Frontend.appController, action: 'poll', interval: 333 });
 				}
 			},
 		   	onFailure: function() {
-				Frontend.appController.get('_timer').set('isPaused', true);
+				Frontend.appController.set('isPolling', false);
 				var closeAction = function() {
 					Frontend.appController.set('tab', 'connection');
 					SC.page.getPath('chatView.chatHistoryScrollView.chatHistoryView').set('innerHTML', '');
@@ -140,7 +145,7 @@ Frontend.appController = SC.Object.create(
 	disconnect: function() {
 		var confirmed = confirm("Are you sure you want to disconnect?");
 		if (!confirmed) { return; }
-		Frontend.appController.get('_timer').set('isPaused', true);
+		Frontend.appController.set('isPolling', false);
 		var request = new Ajax.Request('/backend/close', {
 			method: 'get',
 			evalJS: false,
