505829ba6abc8fa5cff8b89ac677a1953ea743a0
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-08-10 19:02:22 +0000

    maintain 'session' via headers... and some minor cleanup

--- trunk/b2/backend/app/controllers/users.rb
+++ trunk/b2/backend/app/controllers/users.rb
@@ -1,15 +1,15 @@
 class Users < Application
   
   def index
-    hlc = Hotline::client_for(cookies[:_i_hate_u])
-    return render 'Not connected.', :status => 500 if hlc.niL?
+    hlc = Hotline::client_for(client_key)
+    return not_logged_in if hlc.nil?
     render 'Not implemented.'
   end
   
   def info
-    hlc = Hotline::client_for(cookies[:_i_hate_u])
-    return render 'Not logged in.', :status => 500 if hlc.nil?
-    return render 'Invalid parameter.', :status => 500 if params['u'].nil?
+    hlc = Hotline::client_for(client_key)
+    return not_logged_in if hlc.nil?
+    return invalid_params if params['u'].nil?
     hlc.request_user_info(params['u'].to_i)
     # the user info will be sent in an update event... 
     render 'OK'
