1e6b14630350592ef1c25ff00854535409cca7b4
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2008-10-01 02:45:04 +0000

    update merb-generated files to files generated by version 0.9.7 of merb

--- backend/config/init.rb
+++ backend/config/init.rb
@@ -2,6 +2,8 @@ require 'lib/hotline'
 
 HOTLINE_CONNECTIONS = {}
 
+$KCODE = 'UTF8'
+
 #
 # ==== Structure of Merb initializer
 #
@@ -14,11 +16,11 @@ HOTLINE_CONNECTIONS = {}
 # ==== Set up load paths
 #
 
-# Make the app's "gems" directory a place where gems are loaded from.
-# Note that gems directory must have a structure RubyGems uses for
+# Add the app's "gems" directory to the gem load path.
+# Note that the gems directory must mirror the structure RubyGems uses for
 # directories under which gems are kept.
 #
-# To conveniently set it up use gem install -i <merb_app_root/gems>
+# To conveniently set it up, use gem install -i <merb_app_root/gems>
 # when installing gems. This will set up the structure under /gems
 # automagically.
 #
@@ -28,43 +30,54 @@ HOTLINE_CONNECTIONS = {}
 # application to be deployment environment independent. To do so,
 # install gems into merb_app_root/gems directory like this:
 #
-# gem install -i ~/dev/merbapp/gems activesupport-post-2.0.2gem activerecord-post-2.0.2.gem
+# gem install -i merb_app_root/gems activesupport-post-2.0.2.gem activerecord-post-2.0.2.gem
 #
 # Since RubyGems will search merb_app_root/gems for dependencies, order
-# in statement above is important: we need to install ActiveSupport which
+# in the statement above is important: we need to install ActiveSupport which
 # ActiveRecord depends on first.
 #
 # Remember that bundling of dependencies as gems with your application
 # makes it independent of the environment it runs in and is a very
 # good, encouraged practice to follow.
-Gem.clear_paths
-Gem.path.unshift(Merb.root / "gems")
 
 # If you want modules and classes from libraries organized like
 # merbapp/lib/magicwand/lib/magicwand.rb to autoload,
 # uncomment this.
 # Merb.push_path(:lib, Merb.root / "lib") # uses **/*.rb as path glob.
 
-
-
-
 # ==== Dependencies
 
-# These are some examples of how you might specify dependencies.
-# Dependencies load is delayed to one of later Merb app
-# boot stages. It may be important when
-# later part of your configuration relies on libraries specified
-# here.
+# These are a few, but not all, of the standard merb-more dependencies:
 #
-# dependencies "RedCloth", "merb_helpers"
-# OR
-# dependency "RedCloth", "> 3.0"
-# OR
-# dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
-Merb::BootLoader.after_app_loads do
-  # Add dependencies here that must load after the application loads:
+# dependency "merb-action-args"   # Provides support for querystring arguments to be passed in to controller actions
+# dependency "merb-assets"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
+# dependency "merb-cache"         # Provides your application with caching functions 
+# dependency "merb-haml"          # Adds rake tasks and the haml generators to your merb app
+# dependency "merb-jquery"        # Provides a #jquery method to insert jQuery code in to a content block
+# dependency "merb-mailer"        # Integrates mail support via Merb Mailer
 
-  # dependency "magic_admin" # this gem uses the app's model classes
+# These are a few, but not all, of the merb-plugin dependencies:
+#
+# dependency "merb_helpers"           # Provides the form, date/time, and other helpers
+# dependency "merb_param_protection"  # Lets you have better control over your query string params and param logging
+# dependency "merb_stories"           # Provides rspec helper methods for your application
+
+# Miscellaneous dependencies:
+#
+# Specify more than one dependency at a time with the #dependencies method:
+# dependencies "RedCloth", "BlueCloth"
+
+# Specify a specific version of a dependency
+# dependency "RedCloth", "> 3.0"
+
+# Specify more than one dependency at a time as well as the version:
+# dependencies "RedCloth" => "> 3.0", "BlueCloth" => "= 1.0.0"
+
+# You can also add in dependencies after your application loads.
+Merb::BootLoader.after_app_loads do
+  # For example, the magic_admin gem uses the app's model classes. This requires that the models be 
+  # loaded already. So, we can put the magic_admin dependency here:
+  # dependency "magic_admin"
 end
 
 #
@@ -89,51 +102,77 @@ end
 # ==== Pick what you test with
 #
 
-# This defines which test framework the generators will use
-# rspec is turned on by default
+# This defines which test framework the generators will use.
+# RSpec is turned on by default.
 #
-# Note that you need to install the merb_rspec if you want to ue
-# rspec and merb_test_unit if you want to use test_unit.
-# merb_rspec is installed by default if you did gem install
-# merb.
+# To use Test::Unit, you need to install the merb_test_unit gem.
+# To use RSpec, you don't have to install any additional gems, since
+# merb-core provides support for RSpec.
 #
 # use_test :test_unit
-# use_test :rspec
+use_test :rspec
+
+
+#
+# ==== Choose which template engine to use by default
+#
+
+# Merb can generate views for different template engines, choose your favourite as the default.
+
+use_template_engine :erb
+# use_template_engine :haml
 
 
 #
 # ==== Set up your basic configuration
 #
+
+# IMPORTANT:
+#
+# early on Merb boot init file is not yet loaded.
+# Thus setting PORT, PID FILE and ADAPTER using init file does not
+# make sense and only can lead to confusion because default settings
+# will be used instead.
+#
+# Please use command line options for them.
+# See http://wiki.merbivore.com/pages/merb-core-boot-process
+# if you want to know more.
 Merb::Config.use do |c|
 
-  # Sets up a custom session id key, if you want to piggyback sessions of other applications
-  # with the cookie session store. If not specified, defaults to '_session_id'.
+  # Sets up a custom session id key which is used for the session persistence
+  # cookie name.  If not specified, defaults to '_session_id'.
   # c[:session_id_key] = '_session_id'
-
-#  c[:session_secret_key]  = '9cab25655cb53809ee4f5f49d9a5a77c470bd293'
-#  c[:session_store] = 'cookie'
+  
+  # The session_secret_key is only required for the cookie session store.
+  # c[:session_secret_key]  = 'de774cdbd8a8aa3dd265358fa446b464d586de2c'
+  
+  # There are various options here, by default Merb comes with 'cookie', 
+  # 'memory', 'memcache' or 'container'.  
+  # You can of course use your favorite ORM instead: 
+  # 'datamapper', 'sequel' or 'activerecord'.
+  # c[:session_store] = 'cookie'
 end
 
 
 # ==== Tune your inflector
 
-# To fine tune your inflector use word, singular_word and plural_word
-# methods of Language::English::Inflector module metaclass.
+# To fine tune your inflector use the word, singular_word and plural_word
+# methods of English::Inflect module metaclass.
 #
 # Here we define erratum/errata exception case:
 #
-# Language::English::Inflector.word "erratum", "errata"
+# English::Inflect.word "erratum", "errata"
 #
 # In case singular and plural forms are the same omit
 # second argument on call:
 #
-# Language::English::Inflector.word 'information'
+# English::Inflect.word 'information'
 #
 # You can also define general, singularization and pluralization
 # rules:
 #
 # Once the following rule is defined:
-# Language::English::Inflector.rule 'y', 'ies'
+# English::Inflect.rule 'y', 'ies'
 #
 # You can see the following results:
 # irb> "fly".plural
@@ -143,14 +182,14 @@ end
 #
 # Example for singularization rule:
 #
-# Language::English::Inflector.singular_rule 'o', 'oes'
+# English::Inflect.singular_rule 'o', 'oes'
 #
 # Works like this:
 # irb> "heroes".singular
 # => hero
 #
 # Example of pluralization rule:
-# Language::English::Inflector.singular_rule 'fe', 'ves'
+# English::Inflect.singular_rule 'fe', 'ves'
 #
 # And the result is:
 # irb> "wife".plural
