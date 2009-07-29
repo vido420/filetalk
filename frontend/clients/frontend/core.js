07258acae57a40d7f8bff34620012ace5a31f664
Alexei.Svitkine <Alexei.Svitkine@a58d3ec0-0e51-0410-a5e5-f575e0f05fee>
2009-07-29 18:06:30 +0000

    apparently Frontend.server is currently required

--- frontend/clients/frontend/core.js
+++ frontend/clients/frontend/core.js
@@ -4,6 +4,8 @@
 
 Frontend = SC.Object.create({
 
+	server: SC.Server.create({ prefix: ['Frontend'] }),
+
 	// When you are in development mode, this array will be populated with
 	// any fixtures you create for testing and loaded automatically in your
 	// main method.  When in production, this will be an empty array.
